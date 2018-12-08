8535182effde9709c045de344348c7fec59e377a
MOE 365 FTC <moe365ftc@outlook.com>
2018-12-08 11:48:38 -0500

    changing

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/IntegerHolder.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+public class IntegerHolder {
+    public int integer;
+
+    public IntegerHolder(int i) {
+        this.integer = i;
+    }
+}
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/KeysHolder.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+public class KeysHolder {
+    public static final String ROBOT_RADIUS_KEY = "robot_radius_key";
+    public static final String ENCODER_TICS_KEY = "encoder_tics_key";
+    public static final String STRAFE_ENCODER_TICS_KEY = "strafe_encoder_tics_key";
+    public static final String LIFT_ENCODER_TICS_TO_TOP = "lettt";
+    public static final String ASTAR_COORDINATES = "a1x";
+    public static final String DIAGONAL_ENCODER_TICS_KEY = "detkslke";
+
+    public static final int ENCODER_TICS = 72;
+    public static final int STRAFE_ENCODER_TICS = 101;
+    public static final int DIAGONAL_ENCODER_TICS = 112;
+    public static final int LIFT_ENCODER_TICS = 15000;
+    public static final double STRAFE_COUNTER_ACTION = -0.45;
+}
